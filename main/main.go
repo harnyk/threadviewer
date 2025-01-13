974b683bc5548fb96ee31e9dc44c249d7dd49f61
Mark Harnyk <mark.harnyk@namecheap.com>
2025-01-13 14:34:16 +0100

    feat: use cobra and viper

--- main/main.go
+++ main/main.go
@@ -2,32 +2,60 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 	"github.com/sashabaranov/go-openai"
 	"ui"
 )
 
-func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("Usage: threadviewer <thread_id>")
-		return
+var (
+	threadID string
+	apiKey   string
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "threadviewer",
+	Short: "threadviewer is a CLI tool to view threads from OpenAI",
+	Run: func(cmd *cobra.Command, args []string) {
+		client := openai.NewClient(apiKey)
+		thread, err := client.GetThread(threadID)
+		if err != nil {
+			log.Fatalf("Error retrieving thread: %v", err)
+		}
+		
+		markdownContent := formatThreadAsMarkdown(thread)
+		formattedOutput := ui.RenderMarkdown(markdownContent)
+		fmt.Println(formattedOutput)
+	},
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+	rootCmd.PersistentFlags().StringVar(&threadID, "threadID", "", "Thread ID to retrieve")
+	rootCmd.PersistentFlags().StringVar(&apiKey, "apiKey", "", "OpenAI API Key")
+}
+
+func initConfig() {
+	viper.AutomaticEnv()
+	viper.SetEnvPrefix("THREADVIEWER")
+	viper.BindEnv("API_KEY")
+	
+	apiKey = viper.GetString("API_KEY")
+	if apiKey == "" {
+		log.Fatal("API_KEY environment variable is required")
 	}
-
-	threadID := os.Args[1]
-	client := openai.NewClient("your_api_key")
-
-	thread, err := client.GetThread(threadID)
-	if err != nil {
-		fmt.Printf("Error retrieving thread: %v\n", err)
-		return
-	}
-
-	markdownContent := formatThreadAsMarkdown(thread)
-	formattedOutput := ui.RenderMarkdown(markdownContent)
-	fmt.Println(formattedOutput)
 }
 
 func formatThreadAsMarkdown(thread openai.Thread) string {
 	// Implement the formatting logic here
 	return "# Thread Details\n\n" + thread.ID
+}
+
+func main() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 }
